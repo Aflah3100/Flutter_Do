3184251c8c1b5d77db44480f62f925bfafab655b
Aflah Sedhique <aflah3100@gmail.com>
2024-06-27 11:22:36 +0530

    Updated .env file for firebase_options.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_do/firebase/firebase_options.dart';
 import 'package:flutter_do/screens/widgets/authentication_check.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  // Load environment variables
+  await dotenv.load(fileName: "assets/.env"); 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
